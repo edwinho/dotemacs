17fe88efd6c13b45f6604b6d697e9e92ced4846b
Edwin Ho <edwin.z.ho@gmail.com>
2013-02-07 17:07:41 +0800

    .emacs

---
+++ auctex.el
@@ -0,0 +1,36 @@
+;;加载 AucTeX
+(add-to-list 'load-path
+             "~/.emacs.d/auctex-11.87/")
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+;;LaTeX 设置
+(mapc (lambda (mode)
+      (add-hook 'LaTeX-mode-hook mode))
+      (list 'auto-fill-mode
+            'LaTeX-math-mode
+            'turn-on-reftex
+            'linum-mode))
+
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (setq TeX-auto-untabify t     ; remove all tabs before saving
+                  TeX-engine 'xetex       ; use xelatex default
+                  TeX-show-compilation t) ; display compilation windows
+            (TeX-global-PDF-mode t)       ; PDF mode enable, not plain
+            (setq TeX-save-query nil)
+            (imenu-add-menubar-index)
+            (define-key LaTeX-mode-map [f7] 'TeX-complete-symbol)))
+
+;;编译、查看、打印
+(setq TeX-view-program-list (quote (("^pdf$" "." "evince %o %(outpage)"))))
+
+(cond
+ ((eq system-type 'gnu/linux)
+  (add-hook 'LaTeX-mode-hook
+            (lambda ()
+              (setq TeX-view-program-selection '((output-pdf "Evince")
+                                                 ))))))
